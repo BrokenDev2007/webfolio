62c5d734852b5cf3086a2f2c763445865116c97a
Broken Developer <150446481+BrokenDev2007@users.noreply.github.com>
2024-05-30 12:02:43 +0530

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -25,7 +25,7 @@ const CONFIG = {
       },
       manual: {
         // Properties for manually specifying projects
-        projects: ['brokendev2007/SuzumeV1.1', 'brokendev2007/social-tree'], // List of repository names to display. example: ['arifszn/my-project1', 'arifszn/my-project2']
+        projects: ['brokendev2007/SuzumeV1.1', 'brokendev2007/social-tree', 'brokendev2007/firebase-react-drive'], // List of repository names to display. example: ['arifszn/my-project1', 'arifszn/my-project2']
       },
     },
     external: {
